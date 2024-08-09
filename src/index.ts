fd1449c0ca13938bcf90cdaccb714399e36d069a
EsmailAHBAL <usmail.developer@gmail.com>
2024-08-09 11:27:38 +0100

    addin put method

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,16 @@ app.get('/cities', async c => {
   const cities = resp.results
   return c.json(cities)
 })
+app.put("/update/:id", async c => {
+  const body = await c.req.json()
+  const id = c.req.param("id")
+  console.log(id)
+  const resp = await c.env.DB.prepare("update city set name=?1 where id=?2 RETURNING *")
+    .bind(body.name, id)
+    .run()
+  if (resp.success) {
+    return c.json(resp.results)
+  }
+})
 
 export default app
